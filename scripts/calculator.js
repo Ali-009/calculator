bd02056568ea6683190d9c1407139dac4ad2ddf9
AliTinai <ali-009@windowslive.com>
2021-07-09 08:39:42 +0400

    Added the ability to perform basic operations

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -23,8 +23,45 @@ function operate(operator, a, b){
   }
 }
 
-digits = document.querySelectorAll('#digits > button');
-display = document.querySelector('#result');
+let display = document.querySelector('#result');
+let digits = document.querySelectorAll('#digits > button');
+let operations = document.querySelectorAll('#operations > button');
 
+//operands and operator
+let firstNum;
+let secondNum;
+let operator;
+//boolean to check if operation has been clicked
+let opClick;
+
+//Displaying Numbers
 digits.forEach((digit) => digit.addEventListener('click',
-(e) => display.textContent += e.target.textContent ));
+(e) => {
+
+  if(opClick){
+    opClick = false;
+    display.textContent = '';
+  }
+
+  display.textContent += e.target.textContent;
+
+}));
+
+
+operations.forEach((operation) => operation.addEventListener('click',
+(e) => {
+
+  if(!opClick){
+    opClick = true;
+    if(!firstNum){
+      firstNum = display.textContent;
+      operator = e.target.textContent;
+    } else {
+      secondNum = display.textContent;
+      display.textContent = operate(operator, +firstNum, +secondNum);
+      firstNum = display.textContent;
+      operator = e.target.textContent;
+    }
+  }
+
+}));
